a29b06ebf8c7ce722997e440e7d8c1fd2988e4c9
Vu Tien Thanh <vutienthanh250903@gmail.com>
2026-02-07 08:57:56 +0700

    Add cart Controller and empty cart layout

---
+++ Ecommerce-Web/Areas/User/Controllers/CartController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Ecommerce_Web.Areas.User.Controllers
+{
+    public class CartController : Controller
+    {
+        [Area("User")]
+        [Authorize]
+        public IActionResult Index()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var cartItems = new List<object>();
+
+            return View(cartItems);
+        }
+    }
+}
